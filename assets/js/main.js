b3b31f73121e4981deec27067b218ddb6b6056b3
azabraao <az.abraao@gmail.com>
2020-05-06 11:11:21 -0300

    Add apoiadores slide section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 $(".jsSiteHeaderSlide").slick({
   dots: true,
+  prevArrow: $(".site-header__slide-prev"),
+  nextArrow: $(".site-header__slide-next"),
+});
+
+$(".jsApoiadoresSlide").slick({
+  prevArrow: $(".apoiadores__slide-button-prev"),
+  nextArrow: $(".apoiadores__slide-button-next"),
 });
